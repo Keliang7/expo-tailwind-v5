45ad965317de06ac9ec3983d9f211f0179fa15db
liangqi <1739484858@qq.com>
2025-10-20 10:22:51 +0800

    feat: test tailwind

--- App.tsx
+++ App.tsx
@@ -2,10 +2,14 @@ import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import Text from 'react-native-css/components/Text';
 
 export default function App() {
   return (
     <>
+      <Text className="mt-10 bg-yellow-200 text-center text-3xl font-bold underline">
+        Open up App.tsx to start working on your app!
+      </Text>
       <ScreenContent title="Home" path="App.tsx"></ScreenContent>
       <StatusBar style="auto" />
     </>
